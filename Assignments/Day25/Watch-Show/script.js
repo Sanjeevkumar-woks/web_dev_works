ca06972543fbf0119ee468044d466ca2356305d7
Sanjeev_works <sanjeevmanagutti@gmail.com>
2021-11-30 22:16:28 +0530

    adding new project

--- Assignments/Day25/Watch-Show/script.js
+++ Assignments/Day25/Watch-Show/script.js
@@ -4,6 +4,7 @@ var heading=document.getElementById("heading");
 function search(){
 
   var endKey=document.getElementById("search").value;
+  document.getElementById("search").value="";
   heading.innerText=endKey.toUpperCase();
   url="https://api.jikan.moe/v3/search/anime?q="+endKey;
   fetch(url).then((response)=>response.json()).then(function(movies){ renderAllMovies(movies.results)});
@@ -25,12 +26,12 @@ function display_movies(movie){
  card.innerHTML=`
  <img src=${movie.image_url} class="card-img-top" alt="movieimg">
  <div class="card-body">
-   <h5 class="card-title">${movie.title}</h5>
-   <p class="card-text"><span>Rated:${movie.rated}</span><br><span>Likes: #${movie.members}</span><br><span>Score:${movie.score}</span></p>
-   <a href="${movie.url}" class="btn btn-primary ml-2 watch">Watch</a>
+   <h6 class="card-title">${movie.title}</h6>
+   <p class="card-text"><span>Rated:${movie.rated}</span><br><span>Likes: <span id="likes">${movie.members}</span></span><br><span>Score:${movie.score}</span></p>
+ <a href="${movie.url}"> <button id="watch" class="btn btn-primary btn-sm">Watch</button></a>
  </div>
  <div id="like">
- <button id="like-btn" class="btn btn-dark btn-sm"><i class="far fa-thumbs-up"></i>Like</button>
+ <button id="like-btn" class="btn btn-dark btn-sm" onClick="likes()"><i class="far fa-thumbs-up"></i>Like</button>
  </div>
  
  `;
@@ -41,6 +42,9 @@ function renderAllMovies(movies){
   movies.forEach( movie=> display_movies(movie));
 }
 
-
-
+function likes(){
+  const like=document.getElementById("likes");
+  let likenum=parseInt(like.innerText);
+  like.innerText=likenum+1;
+}
 
