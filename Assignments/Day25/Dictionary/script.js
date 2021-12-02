6eae54007fbf32f54d81a19d7f27f1344972e7af
Sanjeev_works <sanjeevmanagutti@gmail.com>
2021-12-02 07:40:26 +0530

    adding new project

--- Assignments/Day25/Dictionary/script.js
+++ Assignments/Day25/Dictionary/script.js
@@ -1,5 +1,4 @@
-
-
+//search function
 function search(){
    let url="https://api.dictionaryapi.dev/api/v2/entries/en/"
     let word= document.getElementById("word").value;
@@ -16,28 +15,45 @@ function search(){
     <span aria-hidden="true">&times;</span>
   </button>
 </div>`;
-              })
+ })
 }
+
+//Displaying the result
 function result(meaning){
     let display=document.getElementById("result");
+    let synonyms1=meaning[0].meanings[0].definitions[0].synonyms;
+    let synonyms2=meaning[0].meanings[0].definitions[1].synonyms;
+
+    //dynamically adding the result
     display.innerHTML=`
-    <h1>${meaning[0].word}</h1>
+    <h1 id="spelling">${meaning[0].word}</h1>
     <p>/${meaning[0].phonetic}/</p>
-
+    
 <ol>
-    <li><h6>Defination: ${meaning[0].meanings[0].definitions[0].definition}</h6>
-    <p>"${meaning[0].meanings[0].definitions[0].example}"</p>
-    <p>Synonyms:${meaning[0].meanings[0].definitions[0].synonyms}</p></li>
+    <li><h6>Defination: <h5>${meaning[0].meanings[0].definitions[0].definition}</h5></h6>
+    <p id="example">"${meaning[0].meanings[0].definitions[0].example}"</p>
+    <p>Synonyms:${synonyms1[0]}, ${synonyms1[1]}, ${synonyms1[2]}, ${synonyms1[3]}</p></li>
 
-    <li><h6>Defination: ${meaning[0].meanings[0].definitions[1].definition}</h6>
-    <p>"${meaning[0].meanings[0].definitions[1].example}"</p> 
-    <p>Synonyms:${meaning[0].meanings[0].definitions[1].synonyms}</p></li>
+    <li><h6>Defination: <h5>${meaning[0].meanings[0].definitions[1].definition}</h5></h6>
+    <p id="example">"${meaning[0].meanings[0].definitions[1].example}"</p> 
+    <p>Synonyms:${synonyms2[0]}, ${synonyms2[1]}, ${synonyms2[2]}, ${synonyms2[3]}</p></li>
 
 </ol>
-<h5>Audio:</h5>
-<audio controls>
-  <source src=${meaning[0].phonetics[0].audio}
-Your browser does not support the audio element.
-</audio>
+<div id="audio">
+   <p>Pronounce:</p>
+    <audio controls>
+    <source src=${meaning[0].phonetics[0].audio}
+    Your browser does not support the audio element.
+    </audio>
+    </div>
     `
 }
+
+//Enter key press eventListner
+var input = document.getElementById("word");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("button-addon2").click();
+  }
+});
--- Assignments/Day25/Watch-Show/script.js
+++ Assignments/Day25/Watch-Show/script.js
@@ -1,24 +1,61 @@
+// Default URL
 var url="https://api.jikan.moe/v3/search/anime?q=cat";
 var display_div=document.getElementById("display");
 var heading=document.getElementById("heading");
+
+//search by the name entered
 function search(){
-
+  // featching the name
   var endKey=document.getElementById("search").value;
-  document.getElementById("search").value="";
-  heading.innerText=endKey.toUpperCase();
-  url="https://api.jikan.moe/v3/search/anime?q="+endKey;
-  fetch(url).then((response)=>response.json()).then(function(movies){ renderAllMovies(movies.results)});
+  //validating the name entered
+  if(endKey==""){
+    let alert=document.getElementById("alert");
+    alert.innerHTML=`
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Enter Movie Name!</strong> 
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>`
+  }
+ else{
+   // Modifying the Url with the end-key entered
+  let curl="https://api.jikan.moe/v3/search/anime?q="+endKey;
+  //Feacthing the data
+  fetch(curl)
+  .then((response)=>response.json())
+  .then(function(movies){ 
+    //seting heading based on the search key
+    document.getElementById("search").value="";
+    heading.innerText=endKey.toUpperCase();
+    renderAllMovies(movies.results)})
+  .catch(()=>{
+    //defalt recommanded movies 
+    recommended()
+    //Displaying alert to handle any errrroe in fetching
+    let alert=document.getElementById("alert");
+      alert.innerHTML=`
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Enter Valid Movie Name!</strong> 
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`
+  })
+ }
 }
 
+//Displaying recommended movies as default
 function recommended(){
-
-heading.innerText="Recommended Movies";
-let session=document.createElement("div");
-session.setAttribute("id","session");
-fetch(url).then((response)=>response.json()).then(function(movies){ renderAllMovies(movies.results)});
-
+  heading.innerText="Recommended Movies";
+  let session=document.createElement("div");
+  session.setAttribute("id","session");
+  fetch(url)
+  .then((response)=>response.json())
+  .then(function(movies){ renderAllMovies(movies.results)})
 }
 
+//Displaying each movie 
 function display_movies(movie){
  const card=document.createElement("div");
  card.setAttribute("class","card col-sm-2");
@@ -37,14 +74,26 @@ function display_movies(movie){
  `;
 display_div.append(card);
 }
+
+//Rendring all movies from the api
 function renderAllMovies(movies){
   display_div.innerHTML="";
   movies.forEach( movie=> display_movies(movie));
 }
 
+//Incrimenting the likes through like button
 function likes(){
   const like=document.getElementById("likes");
   let likenum=parseInt(like.innerText);
   like.innerText=likenum+1;
 }
+
+//Enter key press eventListner
+var input = document.getElementById("search");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("search-btn").click();
+  }
+});
 
