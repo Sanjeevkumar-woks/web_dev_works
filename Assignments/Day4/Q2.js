b30e1e65577e3baca87771c18f29f6497da95eaa
Sanjeev_works <sanjeevmanagutti@gmail.com>
2021-12-03 07:30:43 +0530

    adding new project

---
+++ Assignments/Day4/Q2.js
@@ -0,0 +1,21 @@
+//Set up Http Request
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+xhr.onload = function () {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    var t = JSON.parse(this.response);
+    //Code-Display required data.
+    console.log(t);
+    for (const key in t) {
+      
+
+        console.log(t[key].flag);
+
+        
+    }
+  } 
+  else {
+    console.log("Data is not available");
+  }
+};
+xhr.send();
